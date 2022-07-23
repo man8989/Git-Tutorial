c211b7c200470b9cc831cbe4bdd433357bf82bd5
JMan <ajm890121@gmail.com>
2022-07-23 21:00:18 +0900

    TEST-514

--- testsource.java
+++ testsource.java
@@ -1,7 +1,23 @@
+package com.demo; 
+ 
+import java.util.Iterator; 
+import java.util.LinkedList; 
+import java.util.List; 
+ 
+public class Library { 
+ 
+    private List books; 
+
+     
     /** 
-     * Returns author of this book. 
-     * @wi.implements TEST-512
+     * Searches tgiven word  
+     * in title, author or publisher field. 
+     * Relates to @wi TEST-514 full-text search is needed 
      */ 
-	 
-	 
-int aaa aaaaf
+    public Book[] search(String word) { 
+        List result = new LinkedList(); 
+
+        return (Book[])result.toArray(new Book[result.size()]); 
+    } 
+
+} 
